87052e724135582b0e88d26123a0bf850a756360
Tarasov_I <TIN36@YANDEX.RU>
2022-01-23 19:24:03 +0300

    работа завершена

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
-from utils import get_database, get_film_in_base, movie_search, list_years, years_to_years, rating_db, rating_movie_db
+from utils import get_database, get_film_in_base, movie_search, list_years, years_to_years, rating_db, rating_movie_db, \
+    genre, genre_list, genres
 
 from flask import Flask, render_template, request
 
@@ -85,45 +86,60 @@ def years_movie(start, end):
 
 @app.route('/rating/')
 def category():
-
     return render_template('rating.html')
+
 
 @app.route('/rating/<type>')
 def rating(type):
-
     rating_movie = rating_movie_db()
     movie_list = []
     for i in rating_movie:
         if type == 'children':
             if i[1] == 'G':
                 dict = {
-            "title" : i[0],
-            "rating" : i[1],
-            "description" : i[2]
-        }
+                    "title": i[0],
+                    "rating": i[1],
+                    "description": i[2]
+                }
                 movie_list.append(dict)
         if type == 'family':
             if i[1] == 'G' or i[1] == 'PG-13' or i[1] == 'PG':
                 dict = {
-                        "title": i[0],
-                        "rating": i[1],
-                        "description": i[2]
-                    }
+                    "title": i[0],
+                    "rating": i[1],
+                    "description": i[2]
+                }
                 movie_list.append(dict)
         if type == 'adult':
             if i[1] == 'R' or i[1] == 'NC-17':
                 dict = {
-                        "title": i[0],
-                        "rating": i[1],
-                        "description": i[2]
-                    }
+                    "title": i[0],
+                    "rating": i[1],
+                    "description": i[2]
+                }
                 movie_list.append(dict)
     return render_template('rating.html', movie_list=movie_list)
 
-@app.route('/genre/<genre>')
-def rating(genre):
+
+@app.route('/genre/')
+def genre_list_():
+    genre = genres()
+
+    return render_template('genre.html', genre=genre)
 
 
+@app.route('/genre/<type>')
+def genre_on_type(type):
+    movie = genre(type)
+    movie_list = []
+    for i in movie:
+        dict = {
+            "title": i[0],
+            "rating": i[1],
+            "description": i[2]
+        }
+        movie_list.append(dict)
+    return render_template('genre.html', movie_list=movie_list)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pprint
 import sqlite3
+import json
 
 
 def get_database():
@@ -63,6 +64,7 @@ def years_to_years(start, end):
 
         return db
 
+
 def list_years():
     '''Получение словаря для вывода информации по фильму'''
     with sqlite3.connect("netflix.db") as con:
@@ -85,6 +87,7 @@ def rating_db():
         db = cur.fetchall()
         return db
 
+
 def rating_movie_db():
     '''Получение пары: рейтинг/фильм'''
     with sqlite3.connect("netflix.db") as con:
@@ -96,6 +99,83 @@ def rating_movie_db():
         return db
 
 
+def genre(listed):
+    '''Получение списка фильмов, по заданному жанру'''
+    with sqlite3.connect("netflix.db") as con:
+        cur = con.cursor()
+        query = (
+            f"SELECT title, rating, description FROM netflix WHERE listed_in LIKE '%{listed}%'ORDER BY release_year DESC LIMIT 10")
+        result = cur.execute(query)
+        db = cur.fetchall()
+        return db
 
 
-print(rating_movie_db())
+def genre_list():
+    '''Получение списка с жанрами'''
+    with sqlite3.connect("netflix.db") as con:
+        cur = con.cursor()
+        query = (
+            f"SELECT listed_in FROM netflix GROUP BY listed_in")
+        result = cur.execute(query)
+        db = cur.fetchall()
+        return db
+
+
+def genres():
+    '''Разделяем строку с несколькими жанрами, удаляем дубликаты'''
+    s = genre_list()
+    genre_new = []
+    for i in s:
+        p = i[0].split(', ')
+        for z in p:
+            genre_new.append(z)
+    genres = list(set(genre_new))
+    return genres
+
+
+def cast(name_one, name_two):
+    '''Поиск актеров, которые играли больше двух раз с парой актеров из аргументов'''
+    with sqlite3.connect("netflix.db") as con:
+        cur = con.cursor()
+        query = (
+            f"SELECT netflix.cast FROM netflix WHERE netflix.cast LIKE '%{name_one}%' OR '%{name_two}%'")
+        result = cur.execute(query)
+        db = cur.fetchall()
+        list_cast = []
+        list_friends = []
+        for i in db:
+            p = i[0].split(', ')
+            for z in p:
+                list_cast.append(z)
+        for i in list_cast:
+            if list_cast.count(i) > 2:
+                if i not in list_friends:
+                    list_friends.append(i)
+        list_friends.remove(name_one)
+        list_friends.remove(name_two)
+        return list_friends
+
+
+def type_movie(type_film, year, genre):
+    '''Поиск фильов по критериям'''
+    with sqlite3.connect("netflix.db") as con:
+        dict = {}
+        cur = con.cursor()
+        query = (
+            f"SELECT title, description "
+            f"FROM netflix "
+            f"WHERE type = '{type_film}' AND release_year = '{year}' AND listed_in "
+            f"LIKE '%{genre}%'")
+        result = cur.execute(query)
+        db = cur.fetchall()
+        for i in db:
+            dict[i[0]] = i[1]
+        dict_json = json.dumps(dict)
+
+
+        return dict_json
+
+
+# Проверка функций из шагов 5 и 6, домашнего задания
+print(cast('Jack Black', 'Dustin Hoffman'))
+print(type_movie('TV Show', '2019', 'Action'))
